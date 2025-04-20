7a2020095c32c0b4b5ea7a1399f456b27ea73815
Havva <58465642+halizadehn@users.noreply.github.com>
2025-04-20 21:17:15 +0330

    Update explanation generation.py

--- explanation generation.py
+++ explanation generation.py
@@ -93,140 +93,52 @@ def authority_explanation_generation(user_id, movie_id, persuasiveness_degree):
     return len(winners), persuasiveness_degree
 
 
-def convert_id_to_imdb_id(id):
-    id = str(id)
-    zero_count = 7 - len(id)
-    zero = ""
-    for i in range(zero_count):
-        zero += "0"
-    return "tt" + zero + id
-
-
-def explanation_generation_for_recommendations():
-    df = pd.read_csv(rec_dir + 'kgin_top_10.csv', header=None, index_col=False)
-    df.columns = ["uid", "iid", "rate"]
-    exp_df = pd.DataFrame()
-    for i in range(len(df)):
-        uid = str(df.loc[i, "uid"])
-        iid = str(df.loc[i, "iid"])
-        pred_rate = str(df.loc[i, "rate"])
-        exp, persuade_weight, exp_type = explanation_generation(uid, iid)
-        exp_df.loc[0, "user_id"] = str(uid)
-        exp_df.loc[0, "movie_id"] = str(iid)
-        exp_df.loc[0, "prediction_rate"] = str(pred_rate)
-        exp_df.loc[0, "exp"] = str(exp)
-        exp_df.loc[0, "exp_type"] = str(exp_type)
-        exp_df.loc[0, "persuade_degree"] = str(persuade_weight)
-        exp_df.to_csv(os.path.join(exp_dir + 'explanation_kgin.csv'), mode='a', header=None, index=False)
-
-
-def write_persuasion_profiles():
-    new_df = pd.DataFrame()
-    # df = pd.read_csv("data/users.dat", sep="::", engine='python', header=None)
-    # df.columns = ["uid", "tw_id"]
-    users_df = pd.read_csv('data/dataset/gold_users.csv', index_col=False)
-    users = users_df["user_id"].tolist()
-    for i in range(len(users)):
-        uid = users[i]
-        # result = df.query('uid == ' + str(uid))
-        # tw_id = result['tw_id'].tolist()[0]
-        new_df.loc[i, "user_id"] = str(uid)
-        social, commit, authority = get_persuasion_profile(uid)
-        new_df.loc[i, "social"] = social
-        new_df.loc[i, "commit"] = commit
-        new_df.loc[i, "authority"] = authority
-    new_df.to_csv(os.path.join('data/persuasion_profiles.csv'), mode='w', index=False)
-
-import matplotlib.pyplot as plt
-def stats1():
-    df = pd.read_csv('data/persuasion_profiles.csv', index_col=False)
-    s = df["social"].tolist()
-    c = df["commit"].tolist()
-    a = df["authority"].tolist()
-
-    print(max(s), min(s),max(c), min(c), max(a), min(a))
-    sca = csa = sac = neutral = 0
-    plt.boxplot(s)
-    plt.show()
-    plt.boxplot(c)
-    plt.show()
-    plt.boxplot(a)
-    plt.show()
-    # for i in range(len(df)):
-    #     s = df.loc[i,"social"]
-    #     c = df.loc[i, "commit"]
-    #     a = df.loc[i, "authority"]
-    #     if s == c:
-    #         print(i)
-    #     if c == a:
-    #         print(i)
-        # if s == c == a == 0:
-        #     neutral += 1
-        # elif s >= c and c >= a:
-        #     sca += 1
-        # elif c >= s and s >= a:
-        #     csa += 1
-        # elif s >= a and a >= c:
-        #     sac += 1
-    print(sca, csa, sac, neutral)
-
-
 def get_persuasion_profile(user_id):
     personality = get_user_personality(user_id)
-    social = commit = authority = 0
+    socialproof_score = commitment_score = authority_score = 0
     count = 0
-    if personality[0] == 1: #high_EXT
-        social += 4
-        commit += 3
-        authority += 1
-    if personality[0] == -1: #low_EXT
-        social += 5
-        commit += 3
-        authority += 1
-    if personality[1] == 1: #high_AGR
-        social += 4
-        commit += 3
-        authority += 2
-    if personality[1] == -1: #low_AGR
-        social += 3
-        commit += 4
-        authority += 1
-    if personality[2] == 1: #high_CON
-        social += 3
-        commit += 5
-        authority += 2
-    if personality[2] == -1: #low_CON
-        social += 3
-        commit += 4
-        authority += 1
-    if personality[3] == 1: #high_NEU
-        social += 4
-        commit += 3
-        authority += 1
-    if personality[3] == -1: #low_NEU
-        social += 4
-        commit += 1
-        authority += 3
-    if personality[4] == 1: #high_OPN
-        social += 4
-        commit += 3
-        authority += 2
-    if personality[4] == -1: #low_OPN
-        social += 5
-        commit += 3
-        authority += 1
+    trait_weights = {
+        0: {1: (4, 3, 1), -1: (5, 3, 1)},  # EXT
+        1: {1: (4, 3, 2), -1: (3, 4, 1)},  # AGR
+        2: {1: (3, 5, 2), -1: (3, 4, 1)},  # CON
+        3: {1: (4, 3, 1), -1: (4, 1, 3)},  # NEU
+        4: {1: (4, 3, 2), -1: (5, 3, 1)}   # OPN
+    }
 
-    social = float("{:.2f}".format(social / count))
-    commit = float("{:.2f}".format(commit / count))
-    authority = float("{:.2f}".format(authority / count))
-    return social, commit, authority
-  
-# stats1()
-# write_persuasion_profiles()
-explanation_generation_for_recommendations()
-# df = pd.read_csv(rec_dir + 'fm_top_10.csv', header=None, index_col=False)
-# df.columns = ["uid", "iid", "rate"]
-# x = df["iid"].tolist()
-# x = list(dict.fromkeys(x))
-# print(len(x))
+    for i, value in enumerate(personality):
+        if value in trait_weights[i]:
+            s, c, a = trait_weights[i][value]
+            social += s
+            commit += c
+            authority += a
+            count += 1
 
+    socialproof_score = round(socialproof_score / count, 2)
+    commitment_score = round(commitment_score / count, 2)
+    authority_score = round(authority_score / count, 2)
+    return socialproof_score, commitment_score, authority_score
+
+
+def generate_explanations_for_recommendations(rec_filepath):
+    df = pd.read_csv(rec_filepath, header=None, index_col=False)
+    df.columns = ["uid", "iid", "rate"]
+
+    output_path = os.path.join(data_dir, 'explanations.csv')
+    results = []
+    for _, row in df.iterrows():
+        uid = str(row["uid"])
+        iid = str(row["iid"])
+        pred_rate = str(row["rate"])
+        explanation, persuasiveness_degree, principle = explanation_generation(uid, iid)
+        results.append({
+            "user_id": uid,
+            "movie_id": iid,
+            "prediction_rate": pred_rate,
+            "exp": str(explanation),
+            "exp_type": str(principle),
+            "persuade_degree": str(persuasiveness_degree)
+        })
+
+    exp_df = pd.DataFrame(results)
+    exp_df.to_csv(output_path, mode='a', header=False, index=False)
+    
